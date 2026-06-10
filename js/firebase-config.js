6f22009dfe34a7e6eed2a9540d3fc5c10f1f99ce
tgadzim <tengku.manager@gmail.com>
2026-06-10 14:30:46 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRrUPI-z0JWD-q5UdmLf_7V09zRDHKbYY",
+  authDomain: "eskoba-marketing.firebaseapp.com",
+  projectId: "eskoba-marketing",
+  storageBucket: "eskoba-marketing.firebasestorage.app",
+  messagingSenderId: "940559185905",
+  appId: "1:940559185905:web:f561e9b3b863f737097650",
+  measurementId: "G-9CCPK3Q9K8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
